c69c3a29de444f8260151b10b0827b2745e73145
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-03-19 12:54:42 +0200

    add axios request

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,21 @@
 <template>
     <div class="container">
-
-        <button class="button is-primary" @click="modalActive=true">Open modal</button>
-
-        <modal 
-        :active="modalActive"
-        @close="modalActive=false"
-        src="https://placekitten.com/1280/960"        
-        ></modal>
-
-        <button class="button is-primary" @click="modalActive2=true">Open modal</button>
-
-        <modal 
-        :active="modalActive2"
-        @close="modalActive2=false"
-        src="https://placebear.com/1280/960"        
-        ></modal>
-
-
-        <div class="is-flex mt-3">
-            <input type="text" class="input" placeholder="New item" v-model="newItem" @keypress.enter="addItem">
-            <button class="button is-primary" type="button" @click="addItem">Add Item</button>
-        </div>
-
-        <ul>
-            <li v-for="(item, index) in items" :key="index">{{item}}</li>
-        </ul>
+        <h1 class="is-size-1">{{joke}}</h1>
     </div>
 </template>
 
 <script>
-import Modal from './components/Modal.vue';
+import axios from 'axios';
 export default {
-  components: { Modal },
+    created(){
+        axios.get('https://api.chucknorris.io/jokes/random').then(response => {
+            console.log(response.data.value);
+            this.joke = response.data.value
+        });
+    },
     data(){
         return {
-            message: 'Hello component',
-            newItem: '',
-            items: ['leib', 'sai', 'piim', 'viin'],
-            modalActive: false,
-            modalActive2: false,
-        }
-    },
-    methods: {
-        addItem(){
-            if(this.newItem.trim() !== ''){
-                this.items.push(this.newItem);
-            }
-            this.newItem = '';
+            joke: ''
         }
     }
 }
